ebcc78e19df356595221efb07674a2ad363955e4
mjontop <mjontop0602@gmail.com>
2019-10-21 21:33:16 +0530

    side menu upper added

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,18 @@
 $(document).ready(() => {
-	if (window.innerWidth < 768) {
-		$("li.list-group-item")
-			.not("#mainMenu")
-			.hide()
-		$("#nav").addClass("bg-primary")
-	}
+  if (window.innerWidth < 768) {
+    $("li.list-group-item")
+      .not("#mainMenu")
+      .not("#ragging")
+      .hide()
+    $("#nav").addClass("bg-primary")
+  }
 })
 
 function menuToggler() {
-	if (window.innerWidth < 768) {
-		$("li.list-group-item")
-			.not("#mainMenu")
-			.toggle("slow")
-	}
+  var cls = event.target.id
+  if (window.innerWidth < 768) {
+    $("." + cls)
+      .not("#" + cls)
+      .toggle("slow")
+  }
 }
